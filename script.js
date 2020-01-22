799b470980d901713dcf7a9876aec07440ece12a
maria-roman-garcia <58328976+maria-roman-garcia@users.noreply.github.com>
2020-01-22 14:32:50 +0100

    Subida ejercicio JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let madridStudents = ["Escu", "Angel", "Leire", "Javi", "Cat","Natalia", "Maria"];
+
+for (let student of madridStudents){
+  if (student === "Jara"){
+    console.log("Jara estudia aqui")
+    var jara = true;
+  } 
+};
+
+if (jara === undefined){
+  console.log("oooohhhh Jara no estudia aqui :(")
+};
